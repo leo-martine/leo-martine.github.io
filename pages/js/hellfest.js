adcf9adc7f16b7d6f804c1c1ee38f45a02d68e23
leo <l_martine@cartong.org>
2017-05-19 17:48:52 +0200

    added hall of fame

--- pages/js/hellfest.js
+++ pages/js/hellfest.js
@@ -1,4 +1,51 @@
 $( document ).ready(function() {
+	
+	var bom = {
+		'scene':[
+			{'name':'Discover stage',
+			'value':0.3
+			},
+			{'name':'Gibson stage',
+			'value':0.6
+			},
+			{'name':"Hard'n'heavy stage",
+			'value':0.6
+			},
+			{'name':'Mainstage',
+			'value':1
+			},
+			{'name':'Mainstage 01',
+			'value':1
+			},
+			{'name':'Mainstage 02',
+			'value':1
+			},
+			{'name':'Rock Hard tent',
+			'value':0.6
+			},
+			{'name':'Second stage',
+			'value':1
+			},
+			{'name':'Terrorizer tent',
+			'value':0.6
+			},
+			{'name':'The Altar',
+			'value':0.6
+			},
+			{'name':'The Temple',
+			'value':0.6
+			},
+			{'name':'The Valley',
+			'value':0.6
+			},
+			{'name':'The Warzone',
+			'value':0.6
+			}	
+		]
+	}
+	console.log(bom)
+	
+	
 	$('#map-year-filter').hide()
 	$('#map-style-filter').hide()
 	d3.csv("data/prog.csv", function(prog) {	
@@ -16,7 +63,19 @@ $( document ).ready(function() {
 							prog[i].since = bands[y].since
 						}
 					}
+					
+					// BOM calculation (prend en compte la scene et la duree du concert)
+					if(prog[i].duration && prog[i].stage){
+						var sec = prog[i].duration.split(':')
+						sec = (+sec[0]) * 60 * 60 + (+sec[1]) * 60 + (+sec[2]);
+						for (var z in bom.scene){
+							if (prog[i].stage == bom.scene[z].name){
+								prog[i].bom = sec*bom.scene[z].value;
+							}
+						}
+					}
 				}
+				//console.log(prog)
 				hellfest(prog,bands,countries)
 			})
 		})
@@ -27,6 +86,10 @@ function hellfest(prog,bands,countries){
 	$('.number-bands').html(bands.length+'<br>Groupes') // number of bands
 	$('.number-concerts').html(prog.length+'<br>Concerts') // number of concerts
 	var ndx = crossfilter(prog);
+	var nameDim = ndx.dimension(function(d){return d.band});
+	var nameGroup = nameDim.group();
+	var bomDim = ndx.dimension(function(d){return d.bom});
+	var bomGroup = bomDim.group();
 	var genreDim = ndx.dimension(function(d){return d.style});
 	var genreGroup = genreDim.group();
 	$('.number-genres').html(genreGroup.all().length+'<br>Genres') // number of genres
@@ -36,6 +99,18 @@ function hellfest(prog,bands,countries){
 	
 	var yearDim = ndx.dimension(function(d){return d.year});
 	var yearGroup = yearDim.group(); // year dim
+	
+	// BOM
+	var bomPerBand = nameDim.group().reduceSum(function(d){return d.bom});
+
+	var hallOfFameBands = bomPerBand.top(9)
+	var maxBom = hallOfFameBands[0].value
+	console.log(maxBom)
+	for (var i in hallOfFameBands){
+		var classement = Number(i)+1
+		var percentage = (hallOfFameBands[i].value*100/maxBom).toFixed(1)
+		$('#bomTableBody').append('<tr><td>'+classement+'</td><td>'+hallOfFameBands[i].key+'</td><td><div class="progress"><div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="'+percentage+'" aria-valuemin="0" aria-valuemax="100" style="width:'+percentage+'%">'+percentage+'%</div></div></td></tr>')
+	}
 	
 	// Map
 	
@@ -55,7 +130,6 @@ function hellfest(prog,bands,countries){
 		}
 	}
 	$('.optionMapYear').click(function(){
-		console.log('click')
 		filterMapYear()
 	})
 	
